4e641cb3af7cd8f0644d77630a1782290e458aeb
Daria Roon <dariaroon@yandex.ru>
2023-05-17 07:26:59 +0600

    add parameters to BunTest

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,20 +1,42 @@
 package praktikum;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class BunTest {
+
+    private final String name;
+    private final float price;
+
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] BunData() {
+        return new Object[][]{
+            {"Булочка с кунжутом", 10.0F},
+            {"", -10.0F},
+            {null, 0},
+            {"!@#$%^&*()_+=-1234567890", Float.MIN_VALUE},
+            {"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", Float.MAX_VALUE,}
+        };
+    }
 
     @Test
     public void testGetName() {
-        Bun bun = new Bun("Булочка с кунжутом", 1.0F);
-        assertEquals("Булочка с кунжутом", bun.getName());
+        Bun bun = new Bun(name, price);
+        assertEquals(name, bun.getName());
     }
 
     @Test
     public void testGetPrice() {
-        Bun bun = new Bun("Булочка с кунжутом", 10.0F);
-        assertEquals(10.0F, bun.getPrice(), 0);
+        Bun bun = new Bun(name, price);
+        assertEquals(price, bun.getPrice(), 0);
     }
 }
